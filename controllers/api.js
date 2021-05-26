b0d6c35c300a4ee9f17b1465ed24c2bf5bf1b2a1
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-26 17:56:04 -0400

    we are trying

--- controllers/api.js
+++ controllers/api.js
@@ -3,12 +3,8 @@ const Items = require("../models/Items");
 module.exports = {
   addOrder: async (req, res) => {
     try {
-      let orderObj = JSON.parse(req.body.orders).map((x) => ({
-        id: x,
-      }));
-      const ledio = "60a8934470f7fe07ecc24834";
-      const order = Items.findById(ledio);
-      console.log(order);
+      let orderObj = JSON.parse(req.body.orders).map((x) => ({ _id: x }));
+      const order = Items.find({ $or: orderObj });
       res.render("confirm.ejs", {
         order: order,
         user: req.user,
