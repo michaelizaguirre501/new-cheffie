2e06f7e4c475a5522d22e37ba456632fbfe2fc14
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-26 21:52:51 -0400

    AWAIT YOU GOT TO AWAIT THE RESULTS

--- controllers/api.js
+++ controllers/api.js
@@ -3,12 +3,10 @@ const Items = require("../models/Items");
 module.exports = {
   addOrder: async (req, res) => {
     try {
-      let orderObj = JSON.parse(req.body.orders).map((x) => ({ _id: x }));
-      const order = Items.find({ $or: orderObj });
-      res.render("confirm.ejs", {
-        order: order,
-        user: req.user,
-      });
+      let orderObj = JSON.parse(req.body.orders); //.map((x) => ({ id: x }));
+      const order = await Items.find({ _id: { $in: orderObj } }).lean();
+      console.log(orderObj);
+      res.render("confirm.ejs", { order: order });
     } catch (err) {
       console.log(`ORDER CONTROLLER ${err}`);
     }
