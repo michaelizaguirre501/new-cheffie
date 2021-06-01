bbb03fda3acabbaa0e74cd565999b338f70d7d0a
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-06-01 12:22:31 -0400

    cleaning

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,6 @@ const homeController = require("../controllers/home");
 const itemsController = require("../controllers/items");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/dashboard", ensureAuth, itemsController.getMenu);
 router.get("/login", ensureGuest, authController.getLogin);
@@ -14,4 +13,5 @@ router.get("/logout", ensureAuth, authController.logout);
 router.get("/signup", ensureGuest, authController.getSignup);
 router.post("/signup", ensureGuest, authController.postSignup);
 router.get("/thankYou", homeController.getThankYou);
+
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.post("/addOrder", ensureAuth, ordersController.addOrder);
 router.post("/confirmOrder", ensureAuth, ordersController.confirmOrder);
-router.get("/confirmOrder", ordersController.getOrder);
+router.get("/confirmOrder", ensureAuth, ordersController.getOrder);
 
 module.exports = router;
