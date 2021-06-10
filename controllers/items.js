44921003e0cadf533842df066b215891c85a21ce
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-06-10 17:31:59 -0400

    more css

--- controllers/items.js
+++ controllers/items.js
@@ -43,7 +43,7 @@ module.exports = {
           return acc;
         }, {});
 
-        res.render("createItem.ejs", { orders, counts });
+        res.render("chef-dashboard.ejs", { orders, counts });
       } catch (err) {
         console.log(`ITEMS CONTROLLER ${err}`);
       }
--- routes/items.js
+++ routes/items.js
@@ -4,8 +4,8 @@ const itemsController = require("../controllers/items");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get("/items", itemsController.getMenu);
-router.get("/createItem", ensureAuth, itemsController.getCreateForm);
-router.post("/createItem", ensureAuth, itemsController.createItem);
+router.get("/chef-dashboard", ensureAuth, itemsController.getCreateForm);
+router.post("/chef-dashboard", ensureAuth, itemsController.createItem);
 router.delete("/deleteItem/:id", ensureAuth, itemsController.deleteItem);
 
 module.exports = router;
